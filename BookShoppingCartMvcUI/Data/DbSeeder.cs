8201e9b0f425f79720b0273dde9ea76e0568e7ef
Milan Daki <milandaki234323@gmail.com>
2025-06-30 14:20:41 +0530

    Add project files.

---
+++ BookShoppingCartMvcUI/Data/DbSeeder.cs
@@ -0,0 +1,31 @@
+﻿using BookShoppingCartMvcUI.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUI.Data
+{
+    public class DbSeeder
+    {
+        public static async Task seedDefultData(IServiceProvider service)
+        {
+            var userMgr = service.GetRequiredService<UserManager<IdentityUser>>();
+            var roleMgr = service.GetRequiredService<RoleManager<IdentityRole>>();
+            // adding some role to DB
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            var user = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+            };
+
+            var userInDb = await userMgr.FindByEmailAsync(user.Email);
+            if (userInDb is null)
+            {
+                await userMgr.CreateAsync(user, "Admin@123");
+                await userMgr.AddToRoleAsync(user, Roles.Admin.ToString());
+            }
+        }
+    } 
+}
---
+++ BookShoppingCartMvcUI/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    [Table("Books")]
+    public class Book
+    {      
+        public int  Id { get; set; }
+         
+        [Required]
+        [MaxLength(40)]
+        public string? BookName { get; set; }
+        public double Price { get; set; }
+        public string? Image { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }    
+        public List<OrderDetail> OrderDetails { get; set; }
+        public List<CartDetail> CartDetails { get; set; }
+
+
+    }
+}
